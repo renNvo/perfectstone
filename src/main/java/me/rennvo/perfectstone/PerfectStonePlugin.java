cc61503afaf63846bf4e201cc3717b0d47395631
renNvo <rennvodev@gmail.com>
2022-09-14 14:21:20 +0200

    Initial commit

---
+++ src/main/java/me/rennvo/perfectstone/PerfectStonePlugin.java
@@ -0,0 +1,16 @@
+package me.rennvo.perfectstone;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PerfectStonePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
